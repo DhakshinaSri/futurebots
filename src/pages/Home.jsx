76872c768b8a5e98b9b871bea6564276f0e396e5
Dhakshina Sri <dhakshina2005@gmail.com>
2026-07-05 17:16:35 +0530

    First commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="pt-20">
+        <h1 className="text-4xl font-bold mb-4 text-center">Welcome to FutureBots</h1>
+    </div>
+  );
+}
---
+++ src/pages/Services.jsx
@@ -0,0 +1,7 @@
+export default function Service() {
+  return (
+    <div className="pt-20">
+        <h1 className="text-4xl font-bold mb-4 text-center">Welcome to FutureBots</h1>
+    </div>
+  );
+}
